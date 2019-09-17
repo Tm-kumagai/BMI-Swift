c3a510d514fe0891f794625eb08f369fa0b200a9
t-kumagai <t-kumagai@cries.co.jp>
2019-09-17 11:36:05 +0900

    BMIアプリSwift版作り始め

---
+++ BMI-Swift/BMIApplication/BMIApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BMIApplication
+//
+//  Created by cries on 2019/09/17.
+//  Copyright © 2019年 cries. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
