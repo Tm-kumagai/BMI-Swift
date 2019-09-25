0d14ad3e853bd48e87d6621e99adf246ebefb438
t-kumagai <t-kumagai@cries.co.jp>
2019-09-25 10:19:49 +0900

    入力画面：ラベル変更処理実装

--- BMI-Swift/BMIApplication/BMIApplication/ViewController.swift
+++ BMI-Swift/BMIApplication/BMIApplication/ViewController.swift
@@ -10,14 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var pageTitle: UINavigationBar!
+    @IBOutlet weak var he_title: UILabel!
+    @IBOutlet var he_unit: UILabel!
+    @IBOutlet var we_title: UILabel!
+    @IBOutlet weak var we_unit: UILabel!
     @IBAction func bmiButton(_ sender: UIButton) {
     }
+    @IBOutlet weak var headerTitle: UINavigationBar!
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // title変更
+        headerTitle.items![0].title = "入力"
         
+        // 身長ラベル変更
+        he_title.text = "身長"
+        he_unit.text = "cm"
+        
+        // 体重ラベル変更
+        we_title.text = "体重"
+        we_unit.text = "kg"
         
     }
 
